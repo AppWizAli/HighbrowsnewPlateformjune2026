acb143f7c3f861e78963e4983a9424617ba235b9
AppWizAli <officer3765@gmail.com>
2026-06-05 13:35:08 +0500

    pafsoftware changes

--- pafsoftware/show-users.php
+++ pafsoftware/show-users.php
@@ -97,8 +97,8 @@ $students = $statement->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Students and Results</title>
-    <link rel="stylesheet" href="css/style1.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style1.css">
 </head>
 <body>
     <div class="main">
